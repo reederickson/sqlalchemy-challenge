c826945793d2f1122dd8c26b082155f2b748cfaf
reederickson <reed.erickson19@gmail.com>
2024-01-11 15:04:52 -0800

    begin app setup

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -40,4 +40,46 @@ app = Flask(__name__)
 #################################################
 @app.route("/")
 def welcome():
-    
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"- List of prior year rain totals from all stations<br/>"
+        f"<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"- List of Station numbers and names<br/>"
+        f"<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"- List of prior year temperatures from all stations<br/>"
+        f"<br/>"
+        f"/api/v1.0/start<br/>"
+        f"- When given the start date (YYYY-MM-DD), calculates the MIN/AVG/MAX temperature for all dates greater than and equal to the start date<br/>"
+        f"<br/>"
+        f"/api/v1.0/start/end<br/>"
+        f"- When given the start and the end date (YYYY-MM-DD), calculate the MIN/AVG/MAX temperature for dates between the start and end date inclusive<br/>"
+
+    )
+
+# /api/v1.0/precipitation
+
+# Convert the query results from your precipitation analysis (i.e. retrieve only the last 12 months of data) to a dictionary using date as the key and prcp as the value.
+
+# Return the JSON representation of your dictionary.
+
+# /api/v1.0/stations
+
+# Return a JSON list of stations from the dataset.
+# /api/v1.0/tobs
+
+# Query the dates and temperature observations of the most-active station for the previous year of data.
+
+# Return a JSON list of temperature observations for the previous year.
+
+# /api/v1.0/<start> and /api/v1.0/<start>/<end>
+
+# Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
+
+# For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
+
+# For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
