956d2cf9389aa5c33558187ca3ba92d68e878503
reederickson <reed.erickson19@gmail.com>
2024-01-16 12:50:14 -0800

    case sensitivity change

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -22,10 +22,10 @@ engine= create_engine("sqlite:///Resources.hawaii.sqlite")
 Base=automap_base()
 
 # reflect the tables
-Base.prepare(engine,reflect=True)
+Base.prepare(engine, reflect=True, autoload_with=engine)
 
 # Save references to each table
-Measurement=Base.classes.measurement
+Measurement = Base.classes.measurement
 Station= Base.classes.station 
 
 # Create our session (link) from Python to the DB
@@ -46,7 +46,7 @@ def welcome():
         f"Available Routes:<br/>"
         '''
         Welcome to the Climate Analysis API!  <br/>
-        The Available Routs:
+        The Available Routes:
         /api/v1.0/precipitation
         /api/v1.0/stations
         /api/v1.0/tobs
@@ -58,7 +58,7 @@ def welcome():
 # Convert the query results from your precipitation analysis 
 #(i.e. retrieve only the last 12 months of data) to a dictionary using date as the key and prcp as the value.
 # Return the JSON representation of your dictionary.
-@app.rout("/api/v1.0/precipitation")
+@app.route("/api/v1.0/precipitation")
 def precipitation():
     previous_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     precipitation = session.query(Measurement.date, Measurement.prcp).\
@@ -69,7 +69,7 @@ def precipitation():
 
 #stations
 # Return a JSON list of stations from the dataset.
-@app.rout("/api/v1.0/stations")
+@app.route("/api/v1.0/stations")
 def stations():
     results= session.query(Station.station).all()
     stations=list(np.ravel(results))
@@ -78,7 +78,7 @@ def stations():
 #tobs
 # Query the dates and temperature observations of the most-active station for the previous year of data.
 # Return a JSON list of temperature observations for the previous year.
-@app.rout("/api/v1.0/tobs")
+@app.route("/api/v1.0/tobs")
 def temp_monthly():
     previous_year= dt.date(2017,8,23)- dt.timedelta(days=365)
     results=session.query(Measurement.tobs).\
@@ -91,8 +91,8 @@ def temp_monthly():
 # Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
 # For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
 # For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.# /api/v1.0/<start> and /api/v1.0/<start>/<end>
-@app.rout("/api/v1.0/<start>")
-@app.rout("/api/v1.0/<start>/<end>")
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
 def stats(start=None, end=None):
     sel= [func.min(Measurement.tobs), func.avg(Measurement.tobs),func.max(Measurement.tobs)]
     if not end:
@@ -101,8 +101,12 @@ def stats(start=None, end=None):
             filter(Measurement.date <= end).all()
         temps= list(np.ravel(results))
         return jsonify(temps)
-    results.session.query(*sel).\
+    else:
+        results.session.query(*sel).\
             filter(Measurement.date >= start).\
             filter(Measurement.date <= end).all()
-    temps=list(np.ravel(results))
-    return jsonify(temps=temps)
+        temps=list(np.ravel(results))
+        return jsonify(temps=temps)
+
+if __name__ == '__main__':
+    app.run(debug=False)
